f2530faaa179944cd96e334b07f4d6dc94f1b68e
Domaris Campos <domarisc@gmail.com>
2017-06-04 19:35:56 -0300

    Adicionando regras para o jogo

--- src/pages/newgame/newgame.ts
+++ src/pages/newgame/newgame.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController, NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-game',
@@ -7,9 +7,57 @@ import { NavController } from 'ionic-angular';
 })
 
 export class NewGamePage {
+  qtdJogadas;
+  dadoFisico = false;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
+  }
+
+  jogar() {
+    this.showRadio();
+  }
+
+  showRadio() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Jogar com o dado físico?');
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Sim, salvar para todos os jogos.',
+      value: '1',
+      checked: false
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Sim, salvar para esse jogo.',
+      value: '2',
+      checked: false
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Não, salvar para todos os jogos.',
+      value: '3',
+      checked: true
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Não, salvar para esse jogo.',
+      value: '4',
+      checked: true
+    });
+
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'OK',
+      handler: data => {
+        this.dadoFisico = true;
+      }
+    });
+    alert.present();
   }
 
 }
